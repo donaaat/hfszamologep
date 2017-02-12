c1a5ecb689f3e712b818366c845f189a03ef6475
Dell 7010DT <Dell 7010DT@DELL7010DT-PC>
2017-02-12 21:34:02 +0100

    Calculator osztály

---
+++ számologép HF 1/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace számologép_HF_1
+{
+    class Class1
+    {
+    }
+}
